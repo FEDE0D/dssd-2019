edc2c30752108700fcaff76c5237bd0fc06038fa
Federico Pacheco <federico.g.pacheco@despegar.com>
2019-09-25 21:26:49 -0300

    init

---
+++ videoconf/src/test/java/com/dssd/grupo18/videoconf/VideoconfApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dssd.grupo18.videoconf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VideoconfApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
