5e63197b329d0d5951268dc0702789b59547f630
Federico Pacheco <federico.g.pacheco@despegar.com>
2019-09-28 01:30:57 -0300

    update

--- videoconf/src/main/java/com/dssd/grupo18/videoconf/controller/GenericController.java
+++ videoconf/src/main/java/com/dssd/grupo18/videoconf/controller/GenericController.java
@@ -6,7 +6,6 @@ import com.dssd.grupo18.videoconf.service.GenericService;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -18,13 +17,11 @@ public abstract class GenericController<M extends GenericModel, S extends Generi
         this.service = service;
     }
 
-    @ResponseBody
     @RequestMapping
     public List<M> findAll() {
         return this.service.findAll();
     }
 
-    @ResponseBody
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public M getOne(@PathVariable Long id) {
         return this.service.getOne(id);
--- videoconf/src/main/java/com/dssd/grupo18/videoconf/controller/InternoController.java
+++ videoconf/src/main/java/com/dssd/grupo18/videoconf/controller/InternoController.java
@@ -3,10 +3,10 @@ package com.dssd.grupo18.videoconf.controller;
 import com.dssd.grupo18.videoconf.model.Interno;
 import com.dssd.grupo18.videoconf.service.InternoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping(value = "/interno")
 public class InternoController extends GenericController<Interno, InternoService> {
 
