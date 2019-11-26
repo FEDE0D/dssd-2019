0fbede44f3348202b94d5e0525759474e69cc7cf
Federico Pacheco <federico.g.pacheco@despegar.com>
2019-11-25 21:23:38 -0300

    updates

--- videoconf/src/main/java/com/dssd/grupo18/videoconf/controller/FormsController.java
+++ videoconf/src/main/java/com/dssd/grupo18/videoconf/controller/FormsController.java
@@ -1,34 +1,55 @@
 package com.dssd.grupo18.videoconf.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.bonitasoft.engine.exception.BonitaHomeNotSetException;
+import org.bonitasoft.engine.exception.ServerAPIException;
+import org.bonitasoft.engine.exception.UnknownAPITypeException;
+import org.bonitasoft.engine.platform.LoginException;
 import org.bonitasoft.engine.session.APISession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.dssd.grupo18.videoconf.model.Unidad;
+import com.dssd.grupo18.videoconf.service.BonitaService;
+import com.dssd.grupo18.videoconf.service.UnidadService;
+
 @Controller
 @RequestMapping("/forms")
 public class FormsController {
 
+    @Autowired
+    private UnidadService unidadService;
+
+    @Autowired
+    private BonitaService bonitaService;
+
     @RequestMapping("/login")
-    public String login() {
+    public String login(Model model) {
+        List<Unidad> unidades = unidadService.findAll();
+        model.addAttribute("unidades", unidades);
         return "login";
     }
 
     @RequestMapping("/home")
     public String home(HttpServletRequest request, HttpServletResponse response, Model model) throws IOException {
-        APISession apiSession = (APISession) request.getSession().getAttribute("bonita-session");
-        if (apiSession != null) {
-            model.addAttribute("username", apiSession.getUserName());
+        if (getApiSession(request) != null) {
+            model.addAttribute("username", getApiSession(request).getUserName());
         } else {
             response.sendRedirect("/forms/login");
         }
         return "home";
+    }
+
+    private APISession getApiSession(HttpServletRequest request) {
+        return (APISession) request.getSession().getAttribute("bonita-session");
     }
 
     @RequestMapping("/logout")
@@ -38,8 +59,11 @@ public class FormsController {
     }
 
     @RequestMapping("/solicitud")
-    public String solicitud(@RequestParam(name = "id", required = false) String processInstanceId, Model model) {
+    public String solicitud(@RequestParam(name = "id", required = false) String processInstanceId, Model model,
+        HttpServletRequest request)
+        throws LoginException, ServerAPIException, BonitaHomeNotSetException, UnknownAPITypeException {
         model.addAttribute("processInstanceId", processInstanceId);
+
         return "solicitud";
     }
 
--- videoconf/src/main/webapp/WEB-INF/jsp/login.jsp
+++ videoconf/src/main/webapp/WEB-INF/jsp/login.jsp
@@ -1,3 +1,5 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
@@ -14,3 +16,10 @@
     <button name="submit" type="submit" class="btn btn-primary">Login</button>
   </div>
 </form>
+
+Unidades:
+<ul>
+<c:forEach var="unidad" items="${unidades}" varStatus="counter">
+<li>Unidad ${unidad.id}</li>
+</c:forEach>
+</ul>
